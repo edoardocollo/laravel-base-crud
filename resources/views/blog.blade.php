8a3717975d75b395a141a328257398a581159fd5
edoardo <collo>
2021-02-18 17:36:00 +0100

    initial structure

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>home</title>
+  </head>
+  <body>
+    <h1>blog</h1>
+    <a href="{{route('index')}}">home</a>
+    <a href="{{route('about')}}">about</a>
+    <a href="{{route('blog')}}">blog</a>
+
+  </body>
+</html>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>home</title>
+  </head>
+  <body>
+    <h1>home</h1>
+    <a href="{{route('index')}}">home</a>
+    <a href="{{route('about')}}">about</a>
+    <a href="{{route('blog')}}">blog</a>
+  </body>
+</html>
